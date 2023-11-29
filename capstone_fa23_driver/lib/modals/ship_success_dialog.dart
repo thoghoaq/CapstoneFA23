bd9d52a3b07ddabde41f6bb078a5ca6f2d770e8c
HOANG Trung Thong <thoang5@mantu.com>
2023-11-29 13:35:18 +0700

    fix bug minor

--- capstone_fa23_driver/lib/modals/ship_success_dialog.dart
+++ capstone_fa23_driver/lib/modals/ship_success_dialog.dart
@@ -84,22 +84,25 @@ class _ShipSuccessDialogState extends State<ShipSuccessDialog> {
             children: [
               SvgPicture.asset("assets/images/icons/location.svg"),
               const SizedBox(width: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(nextOrder?.shippingAddress ?? "",
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis),
-                  if (traffic != null)
-                    Text("Cách bạn ${traffic?.distance} km",
-                        style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                              color: DColors.gray2,
-                              fontWeight: FontWeight.w500,
-                            ))
-                ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(nextOrder?.shippingAddress ?? "",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis),
+                    if (traffic != null)
+                      Text("Cách bạn ${traffic?.distance} km",
+                          style:
+                              Theme.of(context).textTheme.bodySmall?.copyWith(
+                                    color: DColors.gray2,
+                                    fontWeight: FontWeight.w500,
+                                  ))
+                  ],
+                ),
               ),
             ],
           )
