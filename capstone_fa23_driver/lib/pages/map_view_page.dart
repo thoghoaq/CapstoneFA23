a1662be020e1f7122ba2581f7c6f809fa16a8281
HOANG Trung Thong <thoang5@mantu.com>
2024-01-18 12:55:08 +0700

    view order

--- capstone_fa23_driver/lib/pages/map_view_page.dart
+++ capstone_fa23_driver/lib/pages/map_view_page.dart
@@ -28,179 +28,160 @@ class _MapViewPageState extends State<MapViewPage> {
   int? distance;
   LatLng? currentLocation;
   Order? nextOrder;
+  bool? isLoading = false;
+  late Map<String, dynamic> order;
 
   Future<Order?> initOrderPage() async {
+    setState(() {
+      isLoading = true;
+    });
     var order = await context.read<OrderProvider>().getOrder(widget.id);
     currentLocation = await LocationHelper().getCurrentLocation();
     // ignore: use_build_context_synchronously
     nextOrder = await context.read<OrderProvider>().getDataOfNextOrder();
+    var orderStr = {
+      "time": DateTimeHelper.getDate(order.expectedShippingDate),
+      "title": order.id,
+      "subtitle": "Được tạo bởi ${order.recipientName}",
+      "code": order.id,
+      "status": order.currentOrderStatus,
+      "sender": {
+        "name": order.senderName,
+        "phone": order.senderPhoneNumber,
+        "avatar": "assets/images/contexts/avatar_2.png",
+        "address": order.senderPhoneNumber,
+      },
+      "receiver": {
+        "name": order.ownerName,
+        "phone": order.ownerPhoneContact,
+        "avatar": "assets/images/contexts/avatar_1.jpg",
+        "address":
+            "${order.shippingAddress}, ${order.shippingWard}, ${order.shippingDistrict}, ${order.shippingProvince}",
+        "distance": order.distanceFromYou,
+      },
+      "lat": order.lat,
+      "lng": order.lng,
+    };
+    setState(() {
+      this.order = orderStr;
+      isLoading = false;
+    });
     return order;
   }
 
   @override
+  void initState() {
+    initOrderPage();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Consumer<OrderProvider>(
-      builder: (context, provider, child) {
-        return Scaffold(
-            appBar: DAppBar(title: "Mã đơn ${widget.id}"),
-            body: FutureBuilder(
-                future: initOrderPage(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData == false) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  var order = {
-                    "time": DateTimeHelper.getDate(
-                        provider.order.expectedShippingDate),
-                    "title": provider.order.id,
-                    "subtitle": "Được tạo bởi ${provider.order.recipientName}",
-                    "code": provider.order.id,
-                    "status": provider.order.currentOrderStatus,
-                    "sender": {
-                      "name": provider.order.senderName,
-                      "phone": provider.order.senderPhoneNumber,
-                      "avatar": "assets/images/contexts/avatar_2.png",
-                      "address": provider.order.senderPhoneNumber,
+    return Scaffold(
+        appBar: DAppBar(title: "Mã đơn ${widget.id}"),
+        body: isLoading == true
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Stack(
+                children: [
+                  GoongMap(
+                    startPoints: currentLocation!,
+                    endPoints: LatLng(order["lat"], order["lng"]),
+                  ),
+                  NotificationListener<DraggableScrollableNotification>(
+                    onNotification:
+                        (DraggableScrollableNotification dsNotification) {
+                      if (dsNotification.extent <= 0.3) {
+                        setState(() {
+                          _displayBottomSheetSize = 0.2;
+                        });
+                      } else if (dsNotification.extent <= 0.6) {
+                        setState(() {
+                          _displayBottomSheetSize = 0.5;
+                        });
+                      } else {
+                        setState(() {
+                          _displayBottomSheetSize = 1;
+                        });
+                      }
+                      return true;
                     },
-                    "receiver": {
-                      "name": provider.order.ownerName,
-                      "phone": provider.order.ownerPhoneContact,
-                      "avatar": "assets/images/contexts/avatar_1.jpg",
-                      "address":
-                          "${provider.order.shippingAddress}, ${provider.order.shippingWard}, ${provider.order.shippingDistrict}, ${provider.order.shippingProvince}",
-                      "distance": provider.order.distanceFromYou,
-                    }
-                  };
-
-                  // LatLng endPoints;
-                  // var startPoints =
-                  //     LatLng(provider.order.lat!, provider.order.lng!);
-                  // if (provider.orders.firstOrNull?.id == provider.order.id) {
-                  //   startPoints = currentLocation!;
-                  //   endPoints =
-                  //       LatLng(provider.order.lat!, provider.order.lng!);
-                  // }
-                  // else {
-                  //   if (nextOrder == null) {
-                  //     startPoints = currentLocation!;
-                  //     endPoints =
-                  //         LatLng(provider.order.lat!, provider.order.lng!);
-                  //   } else {
-                  //     endPoints = LatLng(nextOrder!.lat!, nextOrder!.lng!);
-                  //   }
-                  // }
-
-                  return Stack(
-                    children: [
-                      GoongMap(
-                        startPoints: currentLocation!,
-                        endPoints:
-                            LatLng(provider.order.lat!, provider.order.lng!),
-                      ),
-                      NotificationListener<DraggableScrollableNotification>(
-                        onNotification:
-                            (DraggableScrollableNotification dsNotification) {
-                          if (dsNotification.extent <= 0.3) {
-                            setState(() {
-                              _displayBottomSheetSize = 0.2;
-                            });
-                          } else if (dsNotification.extent <= 0.6) {
-                            setState(() {
-                              _displayBottomSheetSize = 0.5;
-                            });
-                          } else {
-                            setState(() {
-                              _displayBottomSheetSize = 1;
-                            });
-                          }
-                          return true;
-                        },
-                        child: DraggableScrollableSheet(
-                            expand: true,
-                            maxChildSize: 1,
-                            minChildSize: 0.2,
-                            initialChildSize: 0.2,
-                            snap: true,
-                            snapSizes: const [0.2, 0.5, 1],
-                            builder: (BuildContext context,
-                                ScrollController scrollController) {
-                              return Container(
-                                height: 700,
-                                decoration: BoxDecoration(
-                                  color:
-                                      Theme.of(context).colorScheme.background,
-                                  borderRadius: const BorderRadius.vertical(
-                                    top: Radius.circular(8),
-                                  ),
-                                ),
-                                child: ListView(
-                                  controller: scrollController,
+                    child: DraggableScrollableSheet(
+                        expand: true,
+                        maxChildSize: 1,
+                        minChildSize: 0.2,
+                        initialChildSize: 0.2,
+                        snap: true,
+                        snapSizes: const [0.2, 0.5, 1],
+                        builder: (BuildContext context,
+                            ScrollController scrollController) {
+                          return Container(
+                            height: 700,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.background,
+                              borderRadius: const BorderRadius.vertical(
+                                top: Radius.circular(8),
+                              ),
+                            ),
+                            child: ListView(
+                              controller: scrollController,
+                              children: [
+                                Column(
                                   children: [
-                                    Column(
-                                      children: [
-                                        const DSwipeIndicator(),
-                                        if (_displayBottomSheetSize == 1)
-                                          _RoutingWithContact(
-                                            order: order,
-                                            complete: () {
-                                              showDialog(
-                                                  context: context,
-                                                  builder:
-                                                      (BuildContext context) =>
-                                                          ShipSuccessDialog(
-                                                            orderId: widget.id,
-                                                          ));
-                                            },
-                                            cancel: () {
-                                              showDialog(
-                                                  context: context,
-                                                  builder: (BuildContext
-                                                          context) =>
-                                                      const ShipCancelDialog());
-                                            },
-                                          ),
-                                        if (_displayBottomSheetSize == 0.5)
-                                          _Routing(
-                                            order: order,
-                                            complete: () {
-                                              showDialog(
-                                                  context: context,
-                                                  builder:
-                                                      (BuildContext context) =>
-                                                          ShipSuccessDialog(
-                                                            orderId: widget.id,
-                                                          ));
-                                            },
-                                            cancel: () {
-                                              showDialog(
-                                                  context: context,
-                                                  builder: (BuildContext
-                                                          context) =>
-                                                      const ShipCancelDialog());
-                                            },
-                                          ),
-                                        if (_displayBottomSheetSize == 0.2)
-                                          _ReceiverContact(receiver: {
-                                            "name": provider.order.ownerName,
-                                            "phone": provider
-                                                .order.ownerPhoneContact,
-                                            "avatar":
-                                                "assets/images/contexts/avatar_1.jpg"
-                                          }),
-                                      ],
-                                    )
+                                    const DSwipeIndicator(),
+                                    if (_displayBottomSheetSize == 1)
+                                      _RoutingWithContact(
+                                        order: order,
+                                        complete: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) =>
+                                                  ShipSuccessDialog(
+                                                    orderId: widget.id,
+                                                  ));
+                                        },
+                                        cancel: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) =>
+                                                  const ShipCancelDialog());
+                                        },
+                                      ),
+                                    if (_displayBottomSheetSize == 0.5)
+                                      _Routing(
+                                        order: order,
+                                        complete: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) =>
+                                                  ShipSuccessDialog(
+                                                    orderId: widget.id,
+                                                  ));
+                                        },
+                                        cancel: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) =>
+                                                  const ShipCancelDialog());
+                                        },
+                                      ),
+                                    if (_displayBottomSheetSize == 0.2)
+                                      _ReceiverContact(receiver: {
+                                        "name": order["receiver"]["name"],
+                                        "phone": order["receiver"]["phone"],
+                                        "avatar":
+                                            "assets/images/contexts/avatar_1.jpg"
+                                      }),
                                   ],
-                                ),
-                              );
-                            }),
-                      ),
-                    ],
-                  );
-                }));
-      },
-    );
+                                )
+                              ],
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ));
   }
 }
 
